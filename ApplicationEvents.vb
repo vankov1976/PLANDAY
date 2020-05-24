eed07173d5b4c6587f1191965d5f290c2180a5b6
Ventzislav Vankov <vankov1976@gmail.com>
2020-05-25 00:46:33 +0200

    ...

--- ApplicationEvents.vb
+++ ApplicationEvents.vb
@@ -8,19 +8,19 @@ Namespace My
     ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
     ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
     Partial Friend Class MyApplication
-        '      Private Sub AppStart(ByVal sender As Object,
-        'ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) Handles Me.Startup
-        '          AddHandler AppDomain.CurrentDomain.AssemblyResolve, AddressOf ResolveAssemblies
-        '      End Sub
+        Private Sub AppStart(ByVal sender As Object,
+  ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) Handles Me.Startup
+            AddHandler AppDomain.CurrentDomain.AssemblyResolve, AddressOf ResolveAssemblies
+        End Sub
 
-        '      Private Function ResolveAssemblies(sender As Object, e As System.ResolveEventArgs) As Reflection.Assembly
-        '          Dim ressourceName = "PLANDAY." + New Reflection.AssemblyName(e.Name).Name + ".dll"
-        '          If ressourceName = "PLANDAY.itext.licensekey.dll" Then Exit Function
-        '          Using stream = Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(ressourceName)
-        '              Dim assemblyData(CInt(stream.Length)) As Byte
-        '              stream.Read(assemblyData, 0, assemblyData.Length)
-        '              Return Reflection.Assembly.Load(assemblyData)
-        '          End Using
-        '      End Function
+        Private Function ResolveAssemblies(sender As Object, e As System.ResolveEventArgs) As Reflection.Assembly
+            Dim ressourceName = "PLANDAY." + New Reflection.AssemblyName(e.Name).Name + ".dll"
+            If ressourceName = "PLANDAY.itext.licensekey.dll" Then Exit Function
+            Using stream = Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(ressourceName)
+                Dim assemblyData(CInt(stream.Length)) As Byte
+                stream.Read(assemblyData, 0, assemblyData.Length)
+                Return Reflection.Assembly.Load(assemblyData)
+            End Using
+        End Function
     End Class
 End Namespace
